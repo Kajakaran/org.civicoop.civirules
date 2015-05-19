16d90c43a207cc54f1a7e950f4241f747b334d4b
Jaap Jansma <jaap@edeveloper.nl>
2015-05-19 14:29:37 +0200

    added activity contact record type for activity events

---
+++ CRM/CivirulesConditions/Activity/RecordType.mgd.php
@@ -0,0 +1,17 @@
+<?php
+
+return array (
+  0 =>
+    array (
+      'name' => 'Civirules:Condition.ActivityRecordType',
+      'entity' => 'CiviRuleCondition',
+      'params' =>
+        array (
+          'version' => 3,
+          'name' => 'contact_activity_record_type',
+          'label' => 'Activity Contact record type',
+          'class_name' => 'CRM_CivirulesConditions_Activity_RecordType',
+          'is_active' => 1
+        ),
+    ),
+);
---
+++ templates/CRM/CivirulesConditions/Form/Activity/RecordType.tpl
@@ -0,0 +1,11 @@
+<h3>{$ruleConditionHeader}</h3>
+<div class="crm-block crm-form-block crm-civirule-rule_condition-block-activity-type">
+  <div class="crm-section">
+    <div class="label">{$form.record_type_id.label}</div>
+    <div class="content">{$form.record_type_id.html}</div>
+    <div class="clear"></div>
+  </div>
+</div>
+<div class="crm-submit-buttons">
+  {include file="CRM/common/formButtons.tpl" location="bottom"}
+</div>
