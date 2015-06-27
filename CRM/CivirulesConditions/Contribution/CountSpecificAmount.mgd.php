b5b41ee6fc5274cbbce43939176a0f7acbf7d522
Erik Hommel <hommel@ee-atwork.nl>
2015-06-27 15:42:46 +0200

    fix #12 untested

---
+++ CRM/CivirulesConditions/Contribution/CountSpecificAmount.mgd.php
@@ -0,0 +1,17 @@
+<?php
+
+return array (
+  0 =>
+    array (
+      'name' => 'Civirules:Condition.CountSpecificAmount',
+      'entity' => 'CiviRuleCondition',
+      'params' =>
+        array (
+          'version' => 3,
+          'name' => 'count_specific_amount',
+          'label' => 'xth Contribution of Amount xxx',
+          'class_name' => 'CRM_CivirulesConditions_Contribution_CountSpecificAmount',
+          'is_active' => 1
+        ),
+    ),
+);
