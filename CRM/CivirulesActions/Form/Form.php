0f248778bd6c23dd1e06b979175b61a62b254c75
Jaap Jansma <jaap@edeveloper.nl>
2015-07-02 10:47:08 +0200

    fixed #21 notices on action forms

--- CRM/CivirulesActions/Form/Form.php
+++ CRM/CivirulesActions/Form/Form.php
@@ -86,7 +86,7 @@ class CRM_CivirulesActions_Form_Form extends CRM_Core_Form
 
   public function postProcess() {
     $session = CRM_Core_Session::singleton();
-    $session->setStatus('Action '.$this->condition->label.' parameters updated to CiviRule '.$this->rule->label, 'Action parameters updated', 'success');
+    $session->setStatus('Action '.$this->action->label.' parameters updated to CiviRule '.$this->rule->label, 'Action parameters updated', 'success');
 
     $redirectUrl = CRM_Utils_System::url('civicrm/civirule/form/rule', 'action=update&id='.$this->rule->id, TRUE);
     CRM_Utils_System::redirect($redirectUrl);
